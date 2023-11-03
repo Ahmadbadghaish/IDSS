668085ab23c6a4461d235ed7fa55c3800fe37851
ahmad badghaish <badghaish000@gmail.com>
2023-11-03 15:54:51 +0300

    nov 3

---
+++ IDSS-main/frontend/function.js
@@ -0,0 +1,33 @@
+document.addEventListener('DOMContentLoaded', function () {
+  const addBtn = document.querySelector('#addBtn');
+  const container = document.querySelector('#container');
+
+  function createSubpage() {
+    const subpage = document.createElement('div');
+    subpage.classList.add('subpage');
+
+    const title = document.createElement('h2');
+    title.textContent = `Subpage ${container.children.length + 1}`;
+
+    const content = document.createElement('p');
+    content.textContent = 'Content for the subpage. You can put any information here.';
+
+    const redirectBtn = document.createElement('a');
+    redirectBtn.textContent = 'Go to page';
+    redirectBtn.href = 'http://www.example.com'; // Update the URL
+    redirectBtn.classList.add('redirectBtn');
+    redirectBtn.target = '_blank';
+
+    subpage.appendChild(title);
+    subpage.appendChild(content);
+    subpage.appendChild(redirectBtn);
+
+    title.addEventListener('click', function () {
+      content.style.display = content.style.display === 'block' ? 'none' : 'block';
+    });
+
+    container.appendChild(subpage);
+  }
+
+  addBtn.addEventListener('click', createSubpage);
+});
