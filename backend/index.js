2031c58160c6b906744dfb78a84989f7710b7207
jawad-alalasi <98836355+jawad-alalasi@users.noreply.github.com>
2023-12-01 20:22:46 +0300

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,9 @@
 const express = require('express')
 const app = express()
 const port = 3000
-app.get('/', (req, res) => {
-  
-});
- 
-})
+
+app.use('/project',project)
+app.use('/',main)
+
 app.listen(port, () => console.log(`listening on port 
 ${port}!`))
