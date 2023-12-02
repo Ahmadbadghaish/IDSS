8e592fa792edb0bd483e80a6f00eeb6c17c311b3
jawad-alalasi <98836355+jawad-alalasi@users.noreply.github.com>
2023-12-02 10:14:52 +0300

    Update pythonshell.js

--- backend/pythonshell.js
+++ backend/pythonshell.js
@@ -1,22 +1,21 @@
 const express=require('express');
 const app=express();
- 
+const fs = require('fs');
 const {PythonShell} =require('python-shell');
- 
+
+
 app.get("/", (req, res, next)=>{
-    let options = {
-        mode: 'text',
-        pythonOptions: ['-u'], // get print results in real-time 
-        args: ['shubhamk314'] //An argument which can be accessed in the script using sys.argv[1]
-    };
-     
+
+let pyshell = new PythonShell('team2.py', { mode: 'json' });
  
-    PythonShell.run('team2.py', options, function (err, result){
-          if (err) throw err;
-          console.log('result: ', result.toString());
-          res.send(result.toString())
-    });
+fs.createReadStream('data.csv')
+.pipe(csv())
+.on('data', (row) => {
+   pyshell.send(row);
+})
+
 });
  
 const port=8000;
 app.listen(port, ()=>console.log(`Server connected to ${port}`));
+
